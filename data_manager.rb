17e101f883514eac0550713c626527a145dff045
vandosant <sjskender@gmail.com>
2014-04-09 10:55:39 -0600

    Can return an array of companies, and its employees

--- data_manager.rb
+++ data_manager.rb
@@ -8,11 +8,31 @@ class DataManager
     @crm_data[:people].each do |person|
       person[:employments].each do |employment|
         if employment[:company_id] == company_id
-          employees << person
+          titled_person = {
+            :id => person[:id],
+            :first_name => person[:first_name],
+            :last_name => person[:last_name],
+            :title => employment[:title]
+          }
+
+          employees << titled_person
         end
       end
     end
 
     employees
+  end
+
+  def company_list_with_employees
+    new_companies = Array.new
+
+    @crm_data[:companies].each do |company|
+      new_company = {
+        name: company[:name],
+        employees: employees_for_company(company[:id])
+      }
+      new_companies << new_company
+    end
+    new_companies
   end
 end
--- spec/data_manager_spec.rb
+++ spec/data_manager_spec.rb
@@ -37,7 +37,7 @@ describe DataManager do
             [
               {
                 :company_id => 1,
-                :title => "Stunt Man"
+                :title => "Stunt Person"
               }
             ]
         }
@@ -68,15 +68,50 @@ describe DataManager do
         :id => 1,
         :first_name => "Whitmer",
         :last_name => "Jackson",
-        :employments =>
-          [
-            {
-              :company_id => 2,
-              :title => "Technology Manager"
-            }
-          ]
+        :title => "Technology Manager"
       }
     ]
     expect(actual_employees).to match_array(expected_employees)
+  end
+
+  it 'can list the employees for each company' do
+    actual = @data_map.company_list_with_employees
+    expected = [
+      {
+        name: "Buick",
+        employees: []
+      },
+      {
+        name: "Tesla",
+        employees: [
+          {
+            :id => 2,
+            :first_name => "Stewart",
+            :last_name => "Lipton",
+            :title => "Quality Control Engineer"
+          },
+          {
+            :id => 3,
+            :first_name => "Angela",
+            :last_name => "Swanson",
+            :title => "Stunt Person"
+          },
+        ]
+      },
+      {
+        name: "Toyota",
+        employees: [
+          {
+            :id => 1,
+            :first_name => "Whitmer",
+            :last_name => "Jackson",
+            :title => "Technology Manager"
+          }
+        ]
+      }
+
+    ]
+
+    expect(actual).to match_array(expected)
   end
 end
